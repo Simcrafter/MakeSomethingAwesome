b309159d7bce511dfc38998ea1b3e86552db1f7d
Daniel <36774525+Simcrafter@users.noreply.github.com>
2018-05-09 08:53:19 -0700

    Added the Main Python File

---
+++ MakeSomthingAwesome.py
@@ -0,0 +1,23 @@
+#importing modules
+import math as m
+
+#define functions
+
+
+#define variables
+projectileMass=0
+counterweightMass=0
+longArm=0
+shortArm=0
+pivotAngle=0
+slingAngle=0
+pivotAngleCW=0
+heightPivot=0
+slingLength=0
+lengthCW=0
+
+#goals for what to calculate
+#Max Range
+#Velocity at net exit
+#flight time
+
